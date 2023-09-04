1c0146bc88bca207165312380136e3836c5f2956
eunseo <qnrqjrm03@naver.com>
2023-09-04 13:39:27 +0900

    Initial commit

---
+++ backend/src/main/java/com/ssafy/gogosing/GogosingApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.gogosing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogosingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GogosingApplication.class, args);
+	}
+
+}
