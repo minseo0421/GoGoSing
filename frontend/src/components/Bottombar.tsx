d357f7295b87cb95eb30e8af2b7c6c6aa7301105
DobroL33 <dobrolee26@gmail.com>
2023-09-08 16:53:04 +0900

    [S09P22B305-35] fix : 페이지 state 전역 변수화 적용을 통해 뒤로가기 시 에러 해결

--- frontend/src/components/Bottombar.tsx
+++ frontend/src/components/Bottombar.tsx
@@ -1,6 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { useLocation } from "react-router-dom";
-import styled from './bar.module.css';
+import styled from "./bar.module.css";
+
+import { AppState } from "../store/state";
+import { setPage } from "../store/actions";
+import { useSelector, useDispatch } from "react-redux";
 
 interface BottomBarProps {
   onClickButton: (index: number) => void;
@@ -8,7 +12,8 @@ interface BottomBarProps {
 
 const BottomBar: React.FC<BottomBarProps> = ({ onClickButton }) => {
   const location = useLocation();
-  const [isBtn, setBtn] = useState(1);
+  const dispatch = useDispatch();
+  const isPage = useSelector((state: AppState) => state.isPage);
 
   return (
     <>
@@ -18,46 +23,74 @@ const BottomBar: React.FC<BottomBarProps> = ({ onClickButton }) => {
       location.pathname === "/like" ? (
         <div className={styled.Navbar}>
           <div className={styled.iconbar}>
-            <div className={`${styled.iconbtn} ${isBtn === 1 ? styled.sel_icon : null}`} 
+            <div
+              className={`${styled.iconbtn} ${
+                isPage === 1 ? styled.sel_icon : null
+              }`}
               onClick={() => {
                 onClickButton(0);
-                setBtn(1)
-              }}>
-              <img src={`${isBtn===1 ? `assets/sel_home.png`:`assets/unsel_home.png`}`} alt="" />
-              홈
-            </div>
-            <div  className={`${styled.iconbtn} ${isBtn === 2 ? styled.sel_icon : null}`}
-              onClick={() => {
-                onClickButton(1);
-                setBtn(2)
+                dispatch(setPage(1));
               }}
             >
               <img
-                src={`${isBtn===2 ? `assets/sel_chart.png`:`assets/unsel_chart.png`}`}
+                src={`${
+                  isPage === 1 ? `assets/sel_home.png` : `assets/unsel_home.png`
+                }`}
+                alt=""
+              />
+              홈
+            </div>
+            <div
+              className={`${styled.iconbtn} ${
+                isPage === 2 ? styled.sel_icon : null
+              }`}
+              onClick={() => {
+                onClickButton(1);
+                dispatch(setPage(2));
+              }}
+            >
+              <img
+                src={`${
+                  isPage === 2
+                    ? `assets/sel_chart.png`
+                    : `assets/unsel_chart.png`
+                }`}
                 alt=""
               />
               차트
             </div>
-            <div  className={`${styled.iconbtn} ${isBtn === 3 ? styled.sel_icon : null}`}
+            <div
+              className={`${styled.iconbtn} ${
+                isPage === 3 ? styled.sel_icon : null
+              }`}
               onClick={() => {
                 onClickButton(2);
-                setBtn(3)
+                dispatch(setPage(3));
               }}
             >
               <img
-                src={`${isBtn===3 ? `assets/sel_search.png`:`assets/unsel_search.png`}`}
+                src={`${
+                  isPage === 3
+                    ? `assets/sel_search.png`
+                    : `assets/unsel_search.png`
+                }`}
                 alt=""
               />
               검색
             </div>
-            <div  className={`${styled.iconbtn} ${isBtn === 4 ? styled.sel_icon : null}`}
+            <div
+              className={`${styled.iconbtn} ${
+                isPage === 4 ? styled.sel_icon : null
+              }`}
               onClick={() => {
                 onClickButton(3);
-                setBtn(4)
+                dispatch(setPage(4));
               }}
             >
               <img
-                src={`${isBtn===4 ? `assets/sel_like.png`:`assets/unsel_like.png`}`}
+                src={`${
+                  isPage === 4 ? `assets/sel_like.png` : `assets/unsel_like.png`
+                }`}
                 alt=""
               />
               좋아요
--- frontend/src/pages/musiclike.tsx
+++ frontend/src/pages/musiclike.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import { setPage } from "../store/actions";
+import { useDispatch } from "react-redux";
 
 const MusicLike: React.FC = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setPage(4));
+  }, [dispatch]);
   return (
     <div>
       <h1>MusicLike 입니당</h1>
