5790a3af4ba1617c9b3e9ffe81a6c815b8817d72
DobroL33 <dobrolee26@gmail.com>
2023-09-25 17:16:18 +0900

    [S09P22B305-262] feat : musicHome, musicChart, album 더미데이터 이전 완료

--- frontend/src/store/state.tsx
+++ frontend/src/store/state.tsx
@@ -1,4 +1,25 @@
 export interface AppState {
-  isPage:number;
+  isPage: number;
+  // 모달 오픈 (로그인 , 마이페이지, 음악 상세정보)
+  isModalOpen: string | null;
+  // 장르 설문 (마이페이지에서 설문 접근이 가능해서 따로해야함)
+  isGenreSel: boolean;
+
+  // 로그인 성공시 유저 정보
+  isLogin: {
+    socialType: string;
+    nickname: string;
+    gender: string;
+    birth: string;
+    profileImg: string | null;
+  } | null;
+
+  album: {
+    id: number;
+    title: string;
+    singer: string;
+    image: any;
+    url: string;
+    lyrics: string;
+  };
 }
-  
