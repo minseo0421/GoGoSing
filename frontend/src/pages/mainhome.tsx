6487b6c64d4f284da1f514fee497543f6f2e637c
이성민 <lrune2el@naver.com>
2023-09-26 12:28:01 +0900

    [S09P22B305-244] feat : react- 로그인 설정

--- frontend/src/pages/mainhome.tsx
+++ frontend/src/pages/mainhome.tsx
@@ -2,24 +2,25 @@ import React, { useEffect } from "react";
 import MainContainer from "../components/MainContainer/MainContainer";
 import { setPage } from "../store/actions";
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 // import { useLocation } from "react-router-dom";
 import styles from './mainhome.module.css'
 
 const MainHome: React.FC = () => {
-  // const location = useLocation();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
+  const usertoken = localStorage.getItem('usertoken')
   useEffect(() => {
     dispatch(setPage(1));
   }, [dispatch]);
   return (
-    <div style={{height:'85vh'}}>
+    <div style={{height:'88vh'}}>
       <div  className={styles.topbar}>
         <img src="assets/logo.png" alt="" style={{ width: "20%" }} />
-        {/* {usertoken ?  */}
-        {/* <Link style={{ color: "white" }} to="/mypage"><img src="assets/default_user.png" alt="" style={{ width: "80%"}} /></Link>:  */}
+        {usertoken ? 
+        <img src="assets/default_user.png" alt="" style={{ width: "15%"}} onClick={()=>navigate('/mypage')} />: 
         <Link style={{ color: "white" }} to="/login">Login</Link>
-        {/* } */}
+        }
       </div>
       <MainContainer/>
     </div>
