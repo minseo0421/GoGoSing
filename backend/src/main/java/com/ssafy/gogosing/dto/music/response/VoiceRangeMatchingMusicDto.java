a77dd09cb9c0359a57d7be842bb72054777b18ff
eunseo <qnrqjrm03@naver.com>
2023-09-27 02:16:47 +0900

    [S09P22B305-251] feat : 사용자의 음역대 분석 결과 저장 및 반환하기

---
+++ backend/src/main/java/com/ssafy/gogosing/dto/music/response/VoiceRangeMatchingMusicDto.java
@@ -0,0 +1,27 @@
+package com.ssafy.gogosing.dto.music.response;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class VoiceRangeMatchingMusicDto {
+
+    private Long musicId;
+
+    private String singer;
+
+    private String songImg;
+
+    private String title;
+
+    @Builder
+    public VoiceRangeMatchingMusicDto(Long musicId, String singer, String songImg, String title) {
+        this.musicId = musicId;
+        this.singer = singer;
+        this.songImg = songImg;
+        this.title = title;
+    }
+}
