24f0433f98e0e6a78185b1ef54c48534ac2b4aaf
이성민 <lrune2el@naver.com>
2023-09-28 12:47:07 +0900

    [S09P22B305-246] test : youtube load test#4

--- frontend/src/pages/musicsearch.tsx
+++ frontend/src/pages/musicsearch.tsx
@@ -3,7 +3,8 @@ import React, { useEffect } from "react";
 import { setPage } from "../store/actions";
 import { useDispatch } from "react-redux";
 import styles from "./musicsearch.module.css";
-import YouTube from "react-youtube";
+import ReactPlayer from "react-player";
+
 const MusicSearch: React.FC = () => {
   const dispatch = useDispatch();
   useEffect(() => {
@@ -16,22 +17,7 @@ const MusicSearch: React.FC = () => {
       </div>
       <div style={{ height: "75vh", overflow: "auto" }}>
         {/* 여기가 메인 영역 */}
-        <YouTube
-          videoId={"mHHW4_3Z--I"}
-          opts={{
-            width: "100",
-            height: "100",
-            playerVars: {
-              autoplay: 0, //자동재생 O
-              rel: 0, //관련 동영상 표시하지 않음 (근데 별로 쓸모 없는듯..)
-              modestbranding: 1, // 컨트롤 바에 youtube 로고를 표시하지 않음
-            },
-          }}
-          //이벤트 리스너
-          onEnd={(e) => {
-            e.target.stopVideo(0);
-          }}
-        />
+        <ReactPlayer url="https://www.youtube.com/watch?v=pSUydWEqKwE" />
       </div>
     </>
   );
