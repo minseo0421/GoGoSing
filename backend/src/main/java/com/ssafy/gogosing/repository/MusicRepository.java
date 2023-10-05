bd0f62381c69efa89231ea7c99c096260a81e382
minseo0421 <k4496k@naver.com>
2023-10-05 11:15:28 +0900

    [S09P22B305-232] feat: 장르 기반 노래 추천 api

--- backend/src/main/java/com/ssafy/gogosing/repository/MusicRepository.java
+++ backend/src/main/java/com/ssafy/gogosing/repository/MusicRepository.java
@@ -12,4 +12,19 @@ public interface MusicRepository
     Optional<Music> findById(Long musicId);
     @Query("SELECT m FROM MusicGenre mg INNER JOIN Music m ON mg.music.id = m.id WHERE mg.genre.id = :genreId ORDER BY m.viewCount DESC")
     List<Music> findMusicByGenreId(Long genreId);
+
+    @Query(value = "SELECT DISTINCT * " +
+        "FROM (" +
+        "    SELECT m.*, ROW_NUMBER() OVER (ORDER BY m.view_count DESC) AS row_num, RAND() AS random_num " +
+        "    FROM music_genre mg " +
+        "    INNER JOIN music m ON mg.music_id = m.music_id " +
+        "    WHERE mg.genre_id IN (1, 2) " +
+        "    ORDER BY m.view_count DESC " +
+        "    LIMIT 1000" +
+        ") AS subquery " +
+        "ORDER BY subquery.random_num " +
+        "LIMIT 10", nativeQuery = true)
+    List<Music> findTopByPick();
+
+
 }
